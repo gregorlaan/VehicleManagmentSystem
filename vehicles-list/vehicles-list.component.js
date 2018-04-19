f6f1abe96ce232bda36557dd366d9ad89a2ee1f9
testing <testing@Macintosh.local>
2018-04-19 11:10:02 +0300

    Make HTTP recuest

--- vehicles-list/vehicles-list.component.js
+++ vehicles-list/vehicles-list.component.js
@@ -2,6 +2,12 @@ angular.module('vehiclesList').
   component('vehiclesList', {
     templateUrl: 'vehicles-list/vehicles-list.template.html',
     controller: [ '$http',
-      function vehiclesListController($http) {}
+      function vehiclesListController($http) {
+        var self = this;
+        
+        $http.get('data/vehicles.json').then(function(response) {
+          self.vehicles = response.data;
+        });
+      }
     ]
   });
