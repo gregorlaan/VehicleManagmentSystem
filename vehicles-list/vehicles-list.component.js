bb1eebf767d004cb6392dc57044cddc5ce8f99da
testing <testing@Macintosh.local>
2018-04-19 11:40:33 +0300

    Get selected car object

--- vehicles-list/vehicles-list.component.js
+++ vehicles-list/vehicles-list.component.js
@@ -1,10 +1,16 @@
 angular.module('vehiclesList').
   component('vehiclesList', {
     templateUrl: 'vehicles-list/vehicles-list.template.html',
-    controller: [ '$http',
-      function vehiclesListController($http) {
+    controller: [ '$http', '$scope',
+      function vehiclesListController($http, $scope) {
         var self = this;
-        
+        $scope.selectedCar = null;
+
+        $scope.selectCar = function(car) {
+          console.log(car);
+          $scope.selectedCar = car;
+        }
+
         $http.get('data/vehicles.json').then(function(response) {
           self.vehicles = response.data;
         });
