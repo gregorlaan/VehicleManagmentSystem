5ef0c4c85f49f0f36bf1dba40a9b23f3540b3861
Gregor Laan <gregor@github.ee>
2018-04-27 23:22:58 +0300

    Remove unnecessary routing for management

--- angular.config.js
+++ angular.config.js
@@ -6,9 +6,6 @@ angular.module('VMS').
     $routeProvider.
     when('/', {
       template: '<vehicles-list></vehicles-list>'
-    }).
-    when('/management/:vehicleId', {
-      template: '<vehicle-management></vehicle-management>'
     }).otherwise('/')
   }
 ])
